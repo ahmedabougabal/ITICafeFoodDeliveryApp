03c2ab5990cda7c075c840c7fc6b330a5606ccb2
ahmedabougabal <ahmedabougabal@live.com>
2024-09-24 23:16:31 +0300

    feat(menu): adds branch-specific menu items adds many to many relationship between branches and menu_items displays branch specific menu and its items adds class category model which will act as a foreign key class menuItems model implemented and contains every aspect of how an item should be represented (name, price, image, ...)

--- backend/menu/models.py
+++ backend/menu/models.py
@@ -1,3 +1,18 @@
 from django.db import models
-
+from core.models import Branch
 # Create your models here.
+class Category(models.Model):
+    name= models.CharField(max_length=100)
+    def __str__(self):
+        return self.name
+
+class MenuItem(models.Model):
+    name= models.CharField(max_length=200)
+    price= models.DecimalField(max_digits=6, decimal_places=2)
+    description= models.TextField()
+    category= models.ForeignKey(Category, on_delete=models.CASCADE)
+    image = models.ImageField(upload_to='menu_items', blank=True, null=True)
+    branches = models.ManyToManyField(Branch, related_name='menu_items')
+
+    def __str__(self):
+        return self.name
