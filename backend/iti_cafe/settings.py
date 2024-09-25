8d1610e4944a17af25679f5641a75a3504943930
Mahmoud-Wafi <mahmoudwafi33@gmail.com>
2024-09-25 15:25:07 +0300

    updated users auth

--- backend/iti_cafe/settings.py
+++ backend/iti_cafe/settings.py
@@ -189,9 +189,20 @@ AUTH_USER_MODEL = 'users.User'
 
 
 # Looking to send emails in production? Check out our Email API/SMTP product!
-EMAIL_HOST = 'sandbox.smtp.mailtrap.io'
-EMAIL_HOST_USER = 'e51ece82959d02'
-EMAIL_HOST_PASSWORD = 'b2ddf365a55b20'
-DEFAULT_FROM_EMAIL="info@mahmoud.com"
-EMAIL_PORT = '2525'
-EMAIL_USE_TLS= True
+EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
+EMAIL_HOST = "smtp.gmail.com"
+EMAIL_PORT=587
+EMAIL_HOST_USER="mahmoudwafi33@gmail.com"
+EMAIL_HOST_PASSWORD="egjv ffdo kaxm pest"
+EMAIL_USE_TLS=True
+
+
+
+
+
+#EMAIL_HOST = 'sandbox.smtp.mailtrap.io'
+#MAIL_HOST_USER = 'e51ece82959d02'
+#EMAIL_HOST_PASSWORD = 'b2ddf365a55b20'
+#DEFAULT_FROM_EMAIL="info@mahmoud.com"
+#EMAIL_PORT = '2525'
+#EMAIL_USE_TLS= True
