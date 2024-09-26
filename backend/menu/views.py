bbea8160b95a11b8354bbd2a121fdf579cdbda20
ahmedabougabal <ahmedabougabal@live.com>
2024-09-26 23:15:03 +0300

    feat: Introduces menu views and urls for handling business logic and user requests uses serializers to validate and transform data urls to connect views with models and serializers

--- backend/menu/views.py
+++ backend/menu/views.py
@@ -1,3 +1,21 @@
 from django.shortcuts import render
 
 # Create your views here.
+from rest_framework import viewsets
+from .models import MenuItem
+from .serializers import MenuItemSerializer, MenuItemCreateUpdateSerializer
+
+class MenuItemViewSet(viewsets.ModelViewSet):
+    queryset = MenuItem.objects.all()
+    serializer_class = MenuItemSerializer
+
+    def get_serializer_class(self):
+        if self.action in ['create', 'update', 'partial_update']:
+            return MenuItemCreateUpdateSerializer
+        return MenuItemSerializer
+
+    def get_queryset(self):
+        user = self.request.user
+        if user.is_superuser:
+            return MenuItem.objects.all()
+        return MenuItem.objects.filter(branch__name=user.branch)
