1a2a167fe6443deab886183550109c6722be11f5
ahmedabougabal <ahmedabougabal@live.com>
2024-09-26 23:50:24 +0300

    feat: Implement OrderViewSet and configure URLs for orders app
    orderviewset for handling CRUD operations for orders model
    implements get_serializer_class method to user OrderCreateSerializer for create action
    orders url configured in both iti_cafe/urls.py 'api/orders'.

--- backend/orders/views.py
+++ backend/orders/views.py
@@ -1,3 +1,21 @@
-from django.shortcuts import render
+from rest_framework import viewsets
+from .models import Order
+from .serializers import OrderSerializer, OrderCreateSerializer
 
-# Create your views here.
+class OrderViewSet(viewsets.ModelViewSet):
+    queryset = Order.objects.all()
+    serializer_class = OrderSerializer
+
+    def get_serializer_class(self):
+        if self.action == 'create':
+            return OrderCreateSerializer
+        return OrderSerializer
+
+    def get_queryset(self):
+        user = self.request.user
+        if user.is_superuser:
+            return Order.objects.all()
+        return Order.objects.filter(user__branch=user.branch)
+
+    def perform_create(self, serializer):
+        serializer.save(user=self.request.user)
