48f65247faa8a9dd76f1b10523adef0d55e56845
Mahmoud-Wafi <mahmoudwafi33@gmail.com>
2024-09-27 16:38:48 +0300

    user_type field added and phone_number validation

--- backend/iti_cafe/settings.py
+++ backend/iti_cafe/settings.py
@@ -134,13 +134,13 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static'),
-]
+# STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+# STATICFILES_DIRS = [
+#     os.path.join(BASE_DIR, 'static'),
+# ]
 
-MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+# MEDIA_URL = '/media/'
+# MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
@@ -197,7 +197,7 @@ EMAIL_PORT=587
 EMAIL_HOST_USER="mahmoudwafi33@gmail.com"
 EMAIL_HOST_PASSWORD="egjv ffdo kaxm pest"
 EMAIL_USE_TLS=True
-
+DEFAULT_FROM_EMAIL="iticofe@gmail.com"
 
 
 
