8de8de94dc480ef38496efc56d12e80907d1f706
ahmedootta <ahmedfadl3019@gmail.com>
2024-10-07 01:56:59 +0300

    start converting chat feature in react

--- backend/asgi.py
+++ backend/asgi.py
@@ -1,16 +1,16 @@
-"""
-ASGI config for iti_cafe project.
-
-It exposes the ASGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/5.1/howto/deployment/asgi/
-"""
-
 import os
-
 from django.core.asgi import get_asgi_application
+from channels.routing import ProtocolTypeRouter, URLRouter
+from channels.auth import AuthMiddlewareStack
+from rooms import routing  # Replace with your app name
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'iti_cafe.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'iti_cafe.settings')  # Replace with your project settings
 
-application = get_asgi_application()
+application = ProtocolTypeRouter({
+    "http": get_asgi_application(),  # Handle HTTP requests
+    "websocket": AuthMiddlewareStack(  # Handle WebSocket requests
+        URLRouter(
+            routing.websocket_urlpatterns  # Use the URL patterns defined in your routing module
+        )
+    ),
+})
