cce50d8da1ae1621d4c4bf9fab87d8565c1698ac
Mahmoud-Wafi <mahmoudwafi33@gmail.com>
2024-10-06 16:22:32 +0300

    registrtion design added

--- backend/users/migrations/0001_initial.py
+++ backend/users/migrations/0001_initial.py
@@ -12,7 +12,7 @@ class Migration(migrations.Migration):
 
     dependencies = [
         ('auth', '0012_alter_user_first_name_max_length'),
-        ('core', '0001_initial'),
+        ('core', '__first__'),
     ]
 
     operations = [
