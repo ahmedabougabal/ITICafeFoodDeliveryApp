c7e21b92b66c737c61e426cc361ebcb6d5c0ef61
ahmedootta <ahmedfadl3019@gmail.com>
2024-10-07 00:50:21 +0300

    make chat application consist from one static room

--- backend/rooms/routing.py
+++ backend/rooms/routing.py
@@ -2,5 +2,5 @@ from django.urls import path
 from . import consumers
 
 websocket_urlpatterns = [
-    path('ws/<str:room_name>/', consumers.ChatConsumer.as_asgi()),
+    path('ws/<str:chat>/', consumers.ChatConsumer.as_asgi()),
 ]
