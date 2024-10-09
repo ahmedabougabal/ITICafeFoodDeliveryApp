a672b8abde64eca5f62c1a76c2e6a6418187df6e
ahmedootta <ahmedfadl3019@gmail.com>
2024-10-09 04:38:04 +0300

    Handle get all logged users in admin UI

--- backend/rooms/views.py
+++ backend/rooms/views.py
@@ -1,10 +1,4 @@
 from django.shortcuts import render, redirect
-from .models import Message
 
-def room(request):
-    messages = Message.objects.all()
-    return render(request, 'room/chat.html', context={
-        'messages': messages,
-    })
-
+# create your views here
 
