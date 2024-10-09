9001fbec390bb77f2119e60742db921873c75817
ahmedootta <ahmedfadl3019@gmail.com>
2024-10-09 09:50:37 +0300

    Handle open websocket of clicked chat in admin UI & create serializer to begin database works

--- backend/rooms/urls.py
+++ backend/rooms/urls.py
@@ -1,4 +1,7 @@
 from django.urls import path
-from . import views
+from .views import MessageListView
 
-app_name = 'rooms'
+urlpatterns = [
+    path('api/messages/<str:user_email>/<str:admin_email>/', MessageListView.as_view(), name='message-list'),
+    path('api/messages/send/', MessageListView.as_view(), name='send-message'),  # This could be a different view
+]
