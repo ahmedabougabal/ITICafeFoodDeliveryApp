4203b4112346ad4ada9c34ab5dc9bc18ed160fc9
kareem <kareemmorsy555@gmail.com>
2024-10-13 09:30:48 +0300

    created endpoint to notify users by email

--- backend/orders/urls.py
+++ backend/orders/urls.py
@@ -15,4 +15,5 @@ urlpatterns = [
     path('past-orders/', OrderViewSet.as_view({'get': 'past_orders'}), name='order-past-orders'),
     path('<int:pk>/reorder/', OrderViewSet.as_view({'post': 'reorder'}), name='order-reorder'),
     path('latest-order/', OrderViewSet.as_view({'get': 'latest_order'}), name='order-latest'),
+    path('notify-user/', OrderViewSet.as_view({'post': 'notifyUser'}),name='notify-user'),
 ]
