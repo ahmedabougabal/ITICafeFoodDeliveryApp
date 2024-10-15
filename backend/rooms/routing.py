5238fb9224aaf033f676d76505a264711cb8c7c5
ahmedootta <ahmedfadl3019@gmail.com>
2024-10-15 14:43:22 +0300

    conflict resolved

--- backend/rooms/routing.py
+++ backend/rooms/routing.py
@@ -1,6 +1,6 @@
-from django.urls import path
-from . import consumers
+from django.urls import re_path
+from . import consumers 
 
 websocket_urlpatterns = [
-    path('rooms/chat/', consumers.ChatConsumer.as_asgi()),
+    re_path(r'ws/rooms/cafe/$', consumers.ChatConsumer.as_asgi()),  # Ensure this path matches
 ]
